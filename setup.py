292fb9de04f8b0859bf26318f5770c877207d7e2
Wellivelton <veltonmoura@gmail.com>
2022-04-04 20:07:06 -0400

    Consertado o bug na instalação via PyPi
    Este é o release da versão 0.2

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def find_package_data(
 
 
 PACKAGE = "libpythonproVelton"
-NAME = 'libythonprowp'
+NAME = 'libpythonpro-velton' # só aceita nome em letra minuscula
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Wellivelton Pereira"
 AUTHOR_EMAIL = "veltonmoura@gmail.com"
@@ -117,7 +117,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license="GNU Affero General Public License v3 or later (AGPLv3+)",
+    license=read('LICENSE'),
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
